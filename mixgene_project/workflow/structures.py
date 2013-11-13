c18288ad0815addba7c7079c4582b7f3f4c51aa0
Valentin Gologuzov <imvalgo@gmail.com>
2013-11-13 15:26:28 +0100

    Basic structures for block workflow approach.

--- mixgene_project/workflow/structures.py
+++ mixgene_project/workflow/structures.py
@@ -1,7 +1,12 @@
+import gzip
+
 __author__ = 'kost'
 
 import pandas as pd
 import rpy2.robjects as R
+from rpy2.robjects.packages import importr
+from mixgene.settings import R_LIB_CUSTOM_PATH
+
 
 class DataFrameStorage(object):
     def __init__(self, filepath):
@@ -17,7 +22,7 @@ class DataFrameStorage(object):
             @rtype  : pandas.DataFrame
             @return : Stored matrix
         """
-        return pd.from_table(
+        return pd.read_table(
             self.filepath,
             sep=self.sep,
             compression=self.compression,
@@ -32,13 +37,13 @@ class DataFrameStorage(object):
         """
         if not isinstance(df, pd.DataFrame):
             raise TypeError("Given object isn't of DataFrame class: %s" % df)
-        df.to_csv(
-            self.filepath,
-            sep=self.sep,
-            compression=self.compression,
-            header=self.header,
-            index_col=self.index_col,
-        )
+        with gzip.open(self.filepath, "wb") as output:
+            df.to_csv(
+                output,
+                sep=self.sep,
+                header=self.header,
+                index_col=self.index_col,
+            )
 
     def to_r_obj(self):
         """
@@ -93,7 +98,7 @@ class ExpressionSet(object):
         """
         if self.assay_data_storage is None:
             self.assay_data_storage = DataFrameStorage(
-                filepath="%s\%s_assay.csv.gz" % (self.base_dir, self.base_filename))
+                filepath="%s/%s_assay.csv.gz" % (self.base_dir, self.base_filename))
         self.assay_data_storage.store(df)
 
     def get_pheno_data_frame(self):
@@ -104,18 +109,47 @@ class ExpressionSet(object):
             raise RuntimeError("Phenotype data wasn't setup prior")
         return self.pheno_data_storage.load()
 
-
     def store_pheno_data_frame(self, df):
         """
             @type df: DataFrame
         """
         if self.pheno_data_storage is None:
             self.pheno_data_storage = DataFrameStorage(
-                filepath="%s\%s_pheno.csv.gz" % (self.base_dir, self.base_filename))
+                filepath="%s/%s_pheno.csv.gz" % (self.base_dir, self.base_filename))
         self.pheno_data_storage.store(df)
 
     def to_r_obj(self):
         pass
+
+
+class GmtStorage(object):
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.compression = "gzip"
+
+    def load(self):
+        """
+            @rtype  : dict
+            @return : {Set Name -> (Set description, [Set elements])}
+        """
+        gene_sets = {}
+        with gzip.open(self.filepath) as inp:
+            for line in inp:
+                split = line.strip().split("\t")
+                if len(split) < 3:
+                    continue
+                key = split[0]
+                gene_sets[key] = (split[1], split[2:])
+
+        return gene_sets
+
+    def store(self, gene_sets):
+        with gzip.open(self.filepath, "w") as output:
+            for key, rest in gene_sets.iteritems():
+                description, elements = rest
+                output.write("%s\t%s\t%s\n" % (
+                    (key, description, "\t".join(elements))
+                ))
 
 
 class PlatformAnnotation(object):
@@ -132,7 +166,42 @@ class PlatformAnnotation(object):
             @type  base_filename: string
             @param base_filename: Basic name which is used as prefix for all stored data objects
         """
+        self.base_dir = base_dir
+        self.base_filename = base_filename
 
         self.name = platform_name
-        
+        self.gmt_storage = None
 
+        self.gene_units = None
+        self.set_units = None
+
+    def get_gmt(self):
+        if self.gmt_storage is None:
+            raise RuntimeError("Gmt wasn't setup prior")
+        return self.gmt_storage.load()
+
+    def store_gmt(self, gene_sets):
+        """
+            @type  gene_sets: dict
+            @param gene_sets: {Set Name -> (Set description, [Set elements])}
+        """
+        if self.gmt_storage is None:
+            self.gmt_storage = GmtStorage(
+                filepath="%s\%s.gmt.gz" % (self.base_dir, self.base_filename)
+            )
+        self.gmt_storage.store(gene_sets)
+
+    def to_r_obj(self):
+        importr("miXGENE", lib_loc=R_LIB_CUSTOM_PATH)
+        gene_sets = self.load_gmt()
+        r_gene_sets = R.ListVector(dict([
+            (k, R.StrVector(descr_and_elements[1]))
+            for k, descr_and_elements in gene_sets.iteritems()
+        ]))
+
+        mgs = R.r['new']('mixGeneSets')
+        mgs.do_slot_assign("gene.sets", r_gene_sets)
+        mgs.do_slot_assign("org", R.StrVector([self.org]))
+        mgs.do_slot_assign("units", R.StrVector([self.units]))
+
+        return mgs
