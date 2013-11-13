2cd046d72c16ac4a4f5cbd874ee7f54347a0fa94
Valentin Gologuzov <imvalgo@gmail.com>
2013-11-13 15:25:23 +0100

    Script to run project in tmux.

--- run_all.sh
+++ run_all.sh
@@ -1,17 +1,40 @@
+#!/bin/bash
+SESSION=$USER
+
 ### mysql
 # system
 
+### nginx
+# system
 
 ### redis
 # system
 
 ### celery
-python mixgene_project/manage.py celery worker --loglevel=inf &
+#python mixgene_project/manage.py celery worker --loglevel=DEBUG &
 
 ### gunicorn
-gunicorn -b 127.0.0.1:9431 mixgene.wsgi:application &
+#gunicorn -b 127.0.0.1:9431 mixgene.wsgi:application &
 
-### nginx
-# system
+tmux -2 new-session -d -s $SESSION
+
+# Setup a window for tailing log files
+tmux new-window -t $SESSION:1 -n 'WWW-mixgene' "python mixgene_project/manage.py runserver 127.0.0.1:9431"
+tmux send-keys "" C-m
 
 
+# Setup a Celery window
+tmux split-window -h
+tmux resize-pane -R 20
+tmux select-pane -t 1
+tmux send-keys "python mixgene_project/manage.py celery worker --loglevel=INFO" C-m
+tmux split-window -v
+tmux select-pane -t 2
+tmux send-keys "python mixgene_project/manage.py celery flower --port=5555" C-m
+
+tmux select-pane -t 0
+
+# Set default window
+#tmux select-window -t $SESSION:1
+# Attach to session
+tmux -2 attach-session -t $SESSION
