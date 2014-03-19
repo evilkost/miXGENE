fa0aea8fe2c933e8f71a19ecf6cc4116e79a81e8
Valentin Gologuzov <imvalgo@gmail.com>
2014-03-19 21:59:01 +0100

    pretty ModelAdmin for Article

--- mixgene_project/webapp/admin.py
+++ mixgene_project/webapp/admin.py
@@ -4,11 +4,13 @@ from webapp.models import *
 admin.site.register(Experiment)
 admin.site.register(UploadedData)
 admin.site.register(CachedFile)
-admin.site.register(Article)
 
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('dt_created', 'author_user', 'author_title', 'title', 'preview')
+
+admin.site.register(Article, ArticleAdmin)
 
 class BroadInstituteGeneSetAdmin(admin.ModelAdmin):
     list_display = ('section', 'name', 'unit')
-
 
 admin.site.register(BroadInstituteGeneSet, BroadInstituteGeneSetAdmin)
