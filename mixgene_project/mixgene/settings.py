c96a5e7e07e172ddd49c1999f43ce0d092a0933b
Valentin Gologuzov <imvalgo@gmail.com>
2014-04-02 14:00:35 +0200

    [html/css/js] Relocated static files dir to the source root.

--- mixgene_project/mixgene/settings.py
+++ mixgene_project/mixgene/settings.py
@@ -110,7 +110,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    #STATIC_ROOT,
+    SOURCE_DIR + '/static/',
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
